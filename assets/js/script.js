360d6479c722500f5c79d211f57a8795408ab6cf
iamnotpi <ductrung27.dt@gmail.com>
2025-11-24 10:55:14 +0700

    test commit message

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,9 +118,9 @@ for (let i = 0; i < filterBtn.length; i++) {
 // contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
+const formBtn = form.querySelector("[data-form-btn]");
 
-// add event to all form input field
+// add event on keyup
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
@@ -148,7 +148,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
+        window.scrollTo(0, 0);  
       } else {
         pages[i].classList.remove("active");
         navigationLinks[i].classList.remove("active");
